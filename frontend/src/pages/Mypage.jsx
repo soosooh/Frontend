c68124181bd106b2b4aff3a47a88edc99e954c45
sengooooo <lesley023@naver.com>
2024-11-21 21:00:29 +0900

    ✨ FEAT. 마이페이지 추가

--- frontend/src/pages/Mypage.jsx
+++ frontend/src/pages/Mypage.jsx
@@ -1,3 +1,18 @@
-export default function Mypage(){
-    return(<>마이페이지입니다.</>);
+import styled from "styled-components";
+import MyPageComponent from "../entities/mypage/MypageComponent";
+
+export default function Mypage() {
+  return (
+    <Wrapper>
+      <MyPageComponent />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f2f2f2;
+`;
