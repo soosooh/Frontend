1425e7e141b2dd9dc3a5e1bda75956004f8dd371
soohee Jang <shjang0820@hansung.ac.kr>
2024-11-26 00:32:09 +0900

    :sparkles: FEAT. 로그인 API 연결

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -7,4 +7,18 @@ const axiosInstance = axios.create({
   },
 });
 
+// 요청 인터셉터
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken"); // 로컬 스토리지에서 토큰 가져오기
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // 인증 헤더에 토큰 추가
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
